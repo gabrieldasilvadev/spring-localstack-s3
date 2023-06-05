478eab7b63d143e022a032e45b57e163e5a68ccd
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-06-05 01:04:03 -0300

    update file_copy.sh

--- scripts/file_copy.sh
+++ scripts/file_copy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# Copia os arquivos CSV do contêiner para o host
-docker cp localstack:/opt/code/localstack/*.csv ./localstack/files/
+# Compacta os arquivos CSV no contêiner sem o diretório
+docker exec localstack bash -c "tar -czvf /tmp/csv_files.tar.gz -C /opt/code/localstack/ --transform='s#.*/##' *.csv"
+
+# Copia o arquivo compactado para o host
+docker cp localstack:/tmp/csv_files.tar.gz ./files/
+
+# Extrai os arquivos CSV do arquivo compactado
+tar -xzvf ./files/csv_files.tar.gz -C ./files/
+
+# Remove o arquivo compactado
+rm ./files/csv_files.tar.gz
